7429fc2c5d9d0c15f10c6b01a5bea989d4908806
eghm <github-m@eghm.net>
2018-05-30 12:59:21 -1000

    Initial import of project for Stack Overflow question How to resolve Google Service Account Team Drive creation error of insufficientFilePermissions

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # stackoverflow-google-service-account-test-drive-creation
-https://stackoverflow.com/questions/50595511/how-to-resolve-google-service-account-team-drive-creation-error-of-insufficientf
+Project created for Stack Overflow question How to resolve Google Service Account Team Drive creation error of insufficientFilePermissions? [https://stackoverflow.com/questions/50595511/how-to-resolve-google-service-account-team-drive-creation-error-of-insufficientf](https://stackoverflow.com/questions/50595511/how-to-resolve-google-service-account-team-drive-creation-error-of-insufficientf)
+
+I am attempting to create a Google Team Drive using my Service Account but am receiving a insufficientFilePermissions error. Does anyone know how to create a Google Team Drive using a Service Account or can identify what I am doing wrong here?
+
+I've provided the error, Java code for SOTeamDriveCreate, and a pom.xml. One should copy their Service Account credential json file to ServiceAccount.json in the same directory as the pom file. Then execute:
+
+> mvn clean compile ; mvn exec:java -Dexec.mainClass=SOTeamDriveCreate
+
+The insufficientFilePermissions error I would like to resolve:
+```java
+java.lang.reflect.InvocationTargetException
+    at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+    at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+    at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+    at java.lang.reflect.Method.invoke(Method.java:497)
+    at org.codehaus.mojo.exec.ExecJavaMojo$1.run(ExecJavaMojo.java:297)
+    at java.lang.Thread.run(Thread.java:745)
+Caused by: com.google.api.client.googleapis.json.GoogleJsonResponseException: 403 Forbidden
+{
+    "code" : 403,
+    "errors" : [ {
+        "domain" : "global",
+        "message" : "The user does not have sufficient permissions for this file.",
+        "reason" : "insufficientFilePermissions"
+    } ],
+    "message" : "The user does not have sufficient permissions for this file."
+}
+    at com.google.api.client.googleapis.json.GoogleJsonResponseException.from(GoogleJsonResponseException.java:146)
+    at com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest.newExceptionOnError(AbstractGoogleJsonClientRequest.java:113)
+    at com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest.newExceptionOnError(AbstractGoogleJsonClientRequest.java:40)
+    at com.google.api.client.googleapis.services.AbstractGoogleClientRequest$1.interceptResponse(AbstractGoogleClientRequest.java:321)
+    at com.google.api.client.http.HttpRequest.execute(HttpRequest.java:1065)
+    at com.google.api.client.googleapis.services.AbstractGoogleClientRequest.executeUnparsed(AbstractGoogleClientRequest.java:419)
+    at com.google.api.client.googleapis.services.AbstractGoogleClientRequest.executeUnparsed(AbstractGoogleClientRequest.java:352)
+    at com.google.api.client.googleapis.services.AbstractGoogleClientRequest.execute(AbstractGoogleClientRequest.java:469)
+    at SOTeamDriveCreate.createTeamDrive(SOTeamDriveCreate.java:110)
+    at SOTeamDriveCreate.main(SOTeamDriveCreate.java:103)
+    ... 6 more
+```    
---
+++ src/main/java/SOTeamDriveCreate.java
@@ -0,0 +1,113 @@
+    import java.io.FileInputStream;
+    import java.io.IOException;
+    import java.util.Arrays;
+    import java.util.List;
+    import java.util.UUID;
+    
+    import com.google.api.client.auth.oauth2.Credential;
+    import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+    import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+    import com.google.api.client.http.HttpTransport;
+    import com.google.api.client.json.JsonFactory;
+    import com.google.api.client.json.jackson2.JacksonFactory;
+    import com.google.api.client.util.store.FileDataStoreFactory;
+    import com.google.api.services.drive.Drive;
+    import com.google.api.services.drive.DriveScopes;
+    import com.google.api.services.drive.model.TeamDrive;
+    
+    
+    public class SOTeamDriveCreate {
+    
+        /** Application name. */
+        private static final String APPLICATION_NAME =
+                "Drive API Java Quickstart";
+    
+        
+        /** Directory to store user credentials for this application. */
+        private static final java.io.File DATA_STORE_DIR = new java.io.File(
+                System.getProperty("user.home"), ".credentials/drive-java-quickstart");
+    
+        /** Global instance of the {@link FileDataStoreFactory}. */
+        private static FileDataStoreFactory DATA_STORE_FACTORY;
+    
+        /** Global instance of the JSON factory. */
+        private static final JsonFactory JSON_FACTORY =
+                JacksonFactory.getDefaultInstance();
+    
+        /** Global instance of the HTTP transport. */
+        private static HttpTransport HTTP_TRANSPORT;
+    
+        private static Drive service = null;
+    
+        protected static String FILE_FIELDS = "id, name, createdTime, mimeType, trashed, permissions, parents, teamDrive";
+    
+        /**
+         * Global instance of the scopes.
+         *
+         * If modifying these scopes, delete your previously saved credentials
+         * at ~/.credentials/drive-java-quickstart
+         */
+        private static final List<String> SCOPES =
+                Arrays.asList(DriveScopes.DRIVE);
+    
+        private String authFile;
+    
+    //    private DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss");
+    
+        static {
+            try {
+                HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+                DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+            } catch (Throwable t) {
+                t.printStackTrace();
+                System.exit(1);
+            }
+        }
+    
+        public SOTeamDriveCreate(String authFile) {
+            this.authFile = authFile;
+        }
+    
+        /**
+         * Creates an authorized Credential object.
+         *
+         * @return an authorized Credential object.
+         * @throws IOException
+         */
+        public Credential authorize() throws IOException {
+            GoogleCredential credential = GoogleCredential
+                    .fromStream(new FileInputStream(authFile))
+                    .createScoped(Arrays.asList(DriveScopes.DRIVE));
+            return credential;
+        }
+    
+        /**
+         * Build and return an authorized Drive client service.
+         *
+         * @return an authorized Drive client service
+         * @throws IOException
+         */
+        public Drive getDriveService() throws IOException {
+            if (service == null) {
+                Credential credential = authorize();
+                service = new Drive.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, credential)
+                                .setApplicationName(APPLICATION_NAME)
+                                .build();
+            }
+            return service;
+        }
+    
+        public static void main(String[] args) throws Exception {
+            SOTeamDriveCreate createTeamDrive = new SOTeamDriveCreate("ServiceAccount.json");
+            createTeamDrive.createTeamDrive();
+        }
+    
+        public void createTeamDrive() throws Exception {
+            TeamDrive teamDriveMetadata = new TeamDrive();
+            teamDriveMetadata.setName("Team Drive created with Service Account");
+            String requestId = UUID.randomUUID().toString();
+            TeamDrive teamDrive = getDriveService().teamdrives().create(requestId, teamDriveMetadata).setFields(FILE_FIELDS).execute();
+        }
+    
+    }
